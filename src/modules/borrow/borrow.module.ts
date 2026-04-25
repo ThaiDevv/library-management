dcc062dd94e8cb16927f2e3bc7b2b9a968e08886
Thaidev <tranvanthai20062@gmail.com>
2026-04-25 19:57:20 +0700

    feat(borrow): add role-based access control and update borrowing functionality

--- src/modules/borrow/borrow.module.ts
+++ src/modules/borrow/borrow.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BorrowService } from './borrow.service';
 import { BorrowController } from './borrow.controller';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   controllers: [BorrowController],
   providers: [BorrowService],
 })
